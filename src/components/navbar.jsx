046547325def1cdd6b4088bfdb686c59ff3ce20a
Vivek Malhan <vivek.malhan@yahoo.com>
2019-01-10 19:20:09 +0530

    added Textfield and Selectfield

---
+++ src/components/navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Appbar from 'material-ui/AppBar';
+
+const NavBar = () => {
+    return ( 
+        <Appbar title='Image Finder' />
+     );
+}
+ 
+export default NavBar;
